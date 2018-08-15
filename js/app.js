c930c4ceee83b886334180b829796262973a94d1
Timinis <litimc3@gmail.com>
2018-08-15 09:23:23 -0700

    cleared up code after code review

--- js/app.js
+++ js/app.js
@@ -1,56 +1,44 @@
 'use strict';
 
-//Small function
+//Utility Functions
 
 //Start with random number generator;
 const getRndInteger = (min, max) => {
   return Math.floor(Math.random() * (max - min)) + min;
 };
-//function for Sum
-const sumOfNumber = (a, b) => {
-  let sum = a + b;
-  return sum;
-};
+
 //function for adding random cookies to array to the object
-const randomCookiesGenerator = locationObj => {
+const randomCookiesGenerator = ({
+  randomCookies,
+  minCust,
+  maxCust,
+  avgCookies
+}) => {
   for (let i = 0; i < businessHour.length; i++) {
-    locationObj.randomCookies.push(
-      Math.floor(
-        getRndInteger(locationObj.minCust, locationObj.maxCust) *
-        locationObj.avgCookies
-      )
+    randomCookies.push(
+      Math.floor(getRndInteger(minCust, maxCust) * avgCookies)
     );
   }
 };
 //function for summing random cookies as a whole
 const totalCookiesCalc = locationObj => {
-  let sumOfTwo = locationObj.randomCookies[0];
-  for (let i = 1; i < locationObj.randomCookies.length; i++) {
-    sumOfTwo = sumOfNumber(sumOfTwo, locationObj.randomCookies[i]);
+  let sumOfTwo = 0;
+  for (let i = 0; i < locationObj.randomCookies.length; i++) {
+    sumOfTwo = sumOfTwo + locationObj.randomCookies[i];
   }
-  locationObj.totalCookies = sumOfTwo;
+  return sumOfTwo;
 };
-//
-
-//function for object creation
-const locationGenerator = (name, minCust, maxCust, avgCookies, randomCookies) => ({
-  name,
-  minCust,
-  maxCust,
-  avgCookies,
-  randomCookies,
-});
 
 //function  to render the table header
 const headRender = () => {
-  let locationTable = document.getElementById('locationTable');
-  let trEl = document.createElement('tr');
-  let thEl = document.createElement('th');
-  let thEl3 = document.createElement('th');
+  const locationTable = document.getElementById('locationTable');
+  const trEl = document.createElement('tr');
+  const thEl = document.createElement('th');
+  const thEl3 = document.createElement('th');
   thEl.textContent = 'Location';
   trEl.appendChild(thEl);
   for (let i = 0; i < businessHour.length; i++) {
-    let thEl2 = document.createElement('th');
+    const thEl2 = document.createElement('th');
     thEl2.textContent = businessHour[i];
     trEl.appendChild(thEl2);
   }
@@ -60,14 +48,14 @@ const headRender = () => {
 };
 //function to render the table content
 const webRender = locationObj => {
-  let locationTable = document.getElementById('locationTable');
-  let trEl = document.createElement('tr');
-  let tdEl = document.createElement('td');
-  let tdEl3 = document.createElement('td');
+  const locationTable = document.getElementById('locationTable');
+  const trEl = document.createElement('tr');
+  const tdEl = document.createElement('td');
+  const tdEl3 = document.createElement('td');
   tdEl.textContent = locationObj.name;
   trEl.appendChild(tdEl);
   for (let i = 0; i < locationObj.randomCookies.length; i++) {
-    let tdEl2 = document.createElement('td');
+    const tdEl2 = document.createElement('td');
     tdEl2.textContent = locationObj.randomCookies[i];
     trEl.appendChild(tdEl2);
   }
@@ -78,23 +66,23 @@ const webRender = locationObj => {
 
 //function to render the table footer
 const footRender = () => {
-  let locationTable = document.getElementById('locationTable');
-  let trEl = document.createElement('tr');
-  let tdEl = document.createElement('td');
+  const locationTable = document.getElementById('locationTable');
+  const trEl = document.createElement('tr');
+  const tdEl = document.createElement('td');
   tdEl.textContent = 'Total Per Hour';
   trEl.appendChild(tdEl);
   for (let h = 0; h < businessHour.length; h++) {
     let tdEl2 = document.createElement('td');
-    let sumOfTwo = locationList[0].randomCookies[h];
-    for (let i = 1; i < locationList.length; i++) {
-      sumOfTwo = sumOfNumber(sumOfTwo, locationList[i].randomCookies[h]);
+    let sumOfTwo = 0;
+    for (let i = 0; i < locationList.length; i++) {
+      sumOfTwo = sumOfTwo + locationList[i].randomCookies[h];
     }
     tdEl2.textContent = sumOfTwo;
     trEl.appendChild(tdEl2);
   }
-  let tdEl3 = document.createElement('td');
-  let sumOfTwo = locationList[0].totalCookies;
-  for (let i = 1; i < locationList.length; i++) {
+  const tdEl3 = document.createElement('td');
+  let sumOfTwo = 0;
+  for (let i = 0; i < locationList.length; i++) {
     sumOfTwo = locationList[i].totalCookies + sumOfTwo;
   }
   tdEl3.textContent = sumOfTwo;
@@ -102,9 +90,8 @@ const footRender = () => {
   locationTable.appendChild(trEl);
 };
 
-let locationList = [];
 //hours into arrays
-let businessHour = [
+const businessHour = [
   '6 am',
   '7 am',
   '8 am',
@@ -122,23 +109,37 @@ let businessHour = [
   '8 pm'
 ];
 
+//function for object creation
+const locationGenerator = (
+  name,
+  minCust,
+  maxCust,
+  avgCookies,
+  randomCookies
+) => ({
+  name,
+  minCust,
+  maxCust,
+  avgCookies,
+  randomCookies
+});
+
+
+const locationList = [
+  locationGenerator('1st and Pike', 23, 65, 6.3, []),
+  locationGenerator('SeaTac Airport', 3, 24, 1.2, []),
+  locationGenerator('Seattle Center', 11, 38, 3.7, []),
+  locationGenerator('Capitol Hill', 20, 38, 2.3, []),
+  locationGenerator('Alki', 2, 16, 4.6, [])
+];
 
 //creating object and pushing to all-Location array
-let pike = locationGenerator('1st and Pike', 23, 65, 6.3, []);
-locationList.push(pike);
-let seaTac = locationGenerator('SeaTac Airport', 3, 24, 1.2, []);
-locationList.push(seaTac);
-let seaCenter = locationGenerator('Seattle Center', 11, 38, 3.7, []);
-locationList.push(seaCenter);
-let capHill = locationGenerator('Capitol Hill', 20, 38, 2.3, []);
-locationList.push(capHill);
-let alki = locationGenerator('Alki', 2, 16, 4.6, []);
-locationList.push(alki);
+
 
 headRender();
 for (let i = 0; i < locationList.length; i++) {
   randomCookiesGenerator(locationList[i]);
-  totalCookiesCalc(locationList[i]);
+  locationList[i].totalCookies = totalCookiesCalc(locationList[i]);
   webRender(locationList[i]);
 }
 footRender();
